deabfb6658b0024a0e64fd192a7cab049652d563
Vasile Tomoiaga <vasile.tomoiaga@gmail.com>
2021-07-09 16:10:28 +0300

    Updated description

--- JiraQueryFilterSum.user.js
+++ JiraQueryFilterSum.user.js
@@ -2,10 +2,10 @@
 // @name         Jira Query Filter Gadget :: Sum('Any Numeric Column')
 // @namespace    http://tomoiaga.ro
 // @version      0.1
-// @description  try to take over the world!
+// @description  Adds a new Totals row in the header of any Jira issues table with the sum of numeric values in each column.
 // @author       Vasile Tomoiaga
 // @match        *://*/*
-// @icon         https://www.google.com/s2/favicons?domain=rbxd.ds
+// @icon         https://www.google.com/s2/favicons?domain=
 // @grant        none
 // ==/UserScript==
 (function() {
